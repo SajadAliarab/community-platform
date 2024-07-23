f0b43b5054ea54a75afd9b3fb25e6fc51cc9091f
Sajad Aliarab <sajad.aliarab@gmail.com>
2024-07-23 14:49:42 +0100

    try 4 solve amplify problem

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,10 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ["@nuxt/ui"],
-
+  runtimeConfig: {
+    public: {
+      apiUrl: 'http://api-community.sajad.uk'
+    }
+  }
   
 });
